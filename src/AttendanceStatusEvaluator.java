e95f2840f968c3e2e8f98a578722de6c3141cc33
VishnuKarthik007 <vishnukarthikhema@gmail.com>
2026-01-19 15:38:04 +0530

    Fix syntax errors in AttendanceStatusEvaluator

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -2,5 +2,8 @@ import java util.scanner;
 public class attendancestatusevaluator{
   public static void main(String[]args){
     new Scanner sc.nextInt(system.in){
+      int a=100%;
+    if(a>85)
+      System.out.println("
       
     
