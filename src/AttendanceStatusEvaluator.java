14402d19cfa7769252f7ebd00aed5f82488ce884
VishnuKarthik007 <vishnukarthikhema@gmail.com>
2026-01-19 15:55:20 +0530

    Update AttendanceStatusEvaluator.java

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -2,8 +2,7 @@ import java util.scanner;
 public class attendancestatusevaluator{
   public static void main(String[]args){
     new Scanner sc.nextInt(system.in){
-      int a=100;
-    {
+      int a=100
     if(a>85)
       System.out.println("Excellent");
     if-else(a<85&&a>60)
@@ -11,7 +10,6 @@ public class attendancestatusevaluator{
     else
       System.out.println("Poor");
   }
-}
 }
 }
       
