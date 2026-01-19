b96213681c273511dbe7817a1fea6c1166bdfaf0
VishnuKarthik007 <vishnukarthikhema@gmail.com>
2026-01-19 15:43:35 +0530

    Fix attendance evaluation logic and syntax errors

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -2,8 +2,15 @@ import java util.scanner;
 public class attendancestatusevaluator{
   public static void main(String[]args){
     new Scanner sc.nextInt(system.in){
-      int a=100%;
+      int a=100;
     if(a>85)
-      System.out.println("
+      System.out.println("Excellent");
+    if-else(a<85&&a>60)
+      System.out.println("Satisfatory");
+    else
+      System.out.println("Poor");
+  }
+}
+}
       
     
